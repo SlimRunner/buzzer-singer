d747bb983ab1e738841c8cc02826d9a26e7d1dfb
SlimRunner <dv.polygon@gmail.com>
2023-07-19 00:43:35 -0700

    two melodies

--- buzzer-singer.ino
+++ buzzer-singer.ino
@@ -1,20 +1,11 @@
 #include "melody.hpp"
 
-const int duration = getDuration();
+Melody mel(false, 7);
 
 void setup() {
   Serial.begin(9600);
-  initFrequencies();
-  initCumulative();
 }
 
 void loop() {
-  constexpr double conversion = 32 * 128.0 / 60000.0;
-  unsigned long timer = millis() * conversion;
-  int index = findTiming(timer);
-  if (notRests[index]) {
-    tone(7, eqlTemp(notes[index]));
-  } else {
-    noTone(7);
-  }
+  mel.getTone(millis());
 }
