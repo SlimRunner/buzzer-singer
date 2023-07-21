45b1d8642a6719cd051c66ac9f0d8fbce1bb0aba
SlimRunner <dv.polygon@gmail.com>
2023-07-21 00:32:46 -0700

    update interface

--- buzzer-singer.ino
+++ buzzer-singer.ino
@@ -1,22 +1,31 @@
 #include "Melody.hpp"
+#include "Playlist.hpp"
 #include "Toggle.hpp"
 
 constexpr int melodyPin = 2;
 constexpr int switchPin = 7;
 
-// Melody mel(false, melodyPin);
+Melody melodies[2] = {
+    {melodyPin, song1, sizeof(song1) / sizeof(song1[0]), true},
+    {melodyPin, song2, sizeof(song2) / sizeof(song2[0]), true}};
 
 void setup() {
   pinMode(switchPin, INPUT_PULLUP);
   pinMode(melodyPin, INPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
+  constexpr int PLSIZE = static_cast<int>(sizeof(melodies) / sizeof(melodies[0]));
   static Toggle songToggle(switchPin);
-
+  static int songIndex = PLSIZE;
   if (songToggle.update() && songToggle.isHigh()) {
-    // code when press switch has toggled
-    // restart timer of Melody and switch song
+    if (songIndex < PLSIZE) {
+      melodies[songIndex].reset();
+    }
+    songIndex = (songIndex + 1) % (PLSIZE + 1);
   }
-  // update melody
+  if (songIndex < PLSIZE) {
+    melodies[songIndex].play();
+  }
 }
