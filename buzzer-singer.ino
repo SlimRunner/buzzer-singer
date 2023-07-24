c9b42d63dfaeccd4e811116ae317752a1772988c
SlimRunner <dv.polygon@gmail.com>
2023-07-23 22:04:03 -0700

    more cleanup

--- buzzer-singer.ino
+++ buzzer-singer.ino
@@ -8,8 +8,8 @@ constexpr int switchPin = 7;
 Melody melodies[2] = {
     {melodyPin, zelda2, sizeof(zelda2) / sizeof(zelda2[0]), true, 128, 32},
     {melodyPin, passcg, sizeof(passcg) / sizeof(passcg[0]), true, 180, 32}};
-    // {melodyPin, rv443c1, sizeof(rv443c1) / sizeof(rv443c1[0]), false, 120, 32}};
-    // {melodyPin, rv443c2, sizeof(rv443c2) / sizeof(rv443c2[0]), false, 120, 32}};
+// {melodyPin, rv443c1, sizeof(rv443c1) / sizeof(rv443c1[0]), false, 120, 32}};
+// {melodyPin, rv443c2, sizeof(rv443c2) / sizeof(rv443c2[0]), false, 120, 32}};
 
 void setup() {
   pinMode(switchPin, INPUT_PULLUP);
@@ -18,17 +18,17 @@ void setup() {
 }
 
 void loop() {
-  constexpr int PLSIZE =
+  constexpr int PLAYLIST_SIZE =
       static_cast<int>(sizeof(melodies) / sizeof(melodies[0]));
   static Toggle songToggle(switchPin);
-  static int songIndex = PLSIZE;
+  static int songIndex = PLAYLIST_SIZE;
   if (songToggle.isToggled() && songToggle.isHigh()) {
-    if (songIndex < PLSIZE) {
+    if (songIndex < PLAYLIST_SIZE) {
       melodies[songIndex].reset();
     }
-    songIndex = (songIndex + 1) % (PLSIZE + 1);
+    songIndex = (songIndex + 1) % (PLAYLIST_SIZE + 1);
   }
-  if (songIndex < PLSIZE) {
+  if (songIndex < PLAYLIST_SIZE) {
     melodies[songIndex].advance();
   }
 }
