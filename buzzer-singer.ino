a657dafe54f2cca150e89eb482bc69ffb0c69eff
SlimRunner <dv.polygon@gmail.com>
2023-07-26 01:20:32 -0700

    showcase rv443c by default

--- buzzer-singer.ino
+++ buzzer-singer.ino
@@ -5,11 +5,12 @@
 constexpr int melodyPin = 2;
 constexpr int switchPin = 7;
 
-Melody melodies[2] = {
-    {melodyPin, zelda2, sizeof(zelda2) / sizeof(zelda2[0]), true, 128, 32},
-    {melodyPin, passcg, sizeof(passcg) / sizeof(passcg[0]), true, 180, 32}};
-    // {melodyPin, rv443c1, sizeof(rv443c1) / sizeof(rv443c1[0]), false, 120, 32}};
-    // {melodyPin, rv443c2, sizeof(rv443c2) / sizeof(rv443c2[0]), false, 120, 32}};
+Melody melodies[1] = {
+    // {melodyPin, zelda2, sizeof(zelda2) / sizeof(zelda2[0]), true, 128, 32},
+    // {melodyPin, passcg, sizeof(passcg) / sizeof(passcg[0]), true, 180, 32}};
+    {melodyPin, rv443c1, sizeof(rv443c1) / sizeof(rv443c1[0]), false, 120, 120}};
+    // {melodyPin, rv443c2, sizeof(rv443c2) / sizeof(rv443c2[0]), false, 120, 120}};
+    // {melodyPin, rv443c3, sizeof(rv443c3) / sizeof(rv443c3[0]), false, 120, 120}};
 
 void setup() {
   pinMode(switchPin, INPUT_PULLUP);
